8d54b117229cc5b3695ba33eab1c1cb70ef41fc7
wesenseged <wseged@proton.me>
2025-05-20 20:32:23 +0300

    Fix search width

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -5,21 +5,21 @@ import useChatStore from "@/store/chat";
 import { Input } from "./ui/input";
 
 function SearchChat() {
-	const store = useChatStore();
+  const store = useChatStore();
 
-	return (
-		<div className="absolute top-0 flex space-x-4 pt-2 ">
-			<div className="flex items-center justify-between w-full px-2 bg-zinc-100 dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-700 rounded-2xl">
-				<Input
-					value={store.search}
-					onChange={(e) => store.handleSearchChange(e.target.value)}
-					className="bg-zinc-100 dark:bg-zinc-900 border-none focus:outline-none w-64 md:w-full focus:ring-0 focus-visible:ring-0 shadow-none"
-					placeholder="Search..."
-				/>
-				<Search />
-			</div>
-		</div>
-	);
+  return (
+    <div className="absolute top-0 flex space-x-4 pt-2 ">
+      <div className="flex items-center justify-between w-full px-2 bg-zinc-100 dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-700 rounded-2xl">
+        <Input
+          value={store.search}
+          onChange={(e) => store.handleSearchChange(e.target.value)}
+          className="bg-zinc-100 dark:bg-zinc-900 border-none focus:outline-none w-40 md:w-full focus:ring-0 focus-visible:ring-0 shadow-none"
+          placeholder="Search..."
+        />
+        <Search />
+      </div>
+    </div>
+  );
 }
 
 export default SearchChat;
